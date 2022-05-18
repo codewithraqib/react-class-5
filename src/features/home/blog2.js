496cbb99b161f0c09e8c241f8788e3e6a0837399
unknown <bhatashu666@gmail.com>
2022-05-18 23:08:50 +0530

    blog2 added

---
+++ src/features/home/blog2.js
@@ -0,0 +1,140 @@
+import React from 'react';
+
+
+class BlogPage extends React.PureComponent{
+
+
+
+    render(){
+
+        return(
+            <div className="blog2-main-container">
+           
+           <div className='left-blog'> 
+           <div className='blog1-logo'>
+           <img src="/assets/images/logo.png" alt="" />
+           </div>
+           <div className='left-blog-links'>
+             <div><img src="/assets/images/3916580.png" alt="" /></div>
+             <div><img src="/assets/images/6422200.png" alt="" /></div>
+             <div><img src="/assets/images/3916631.png" alt="" /></div>
+             <div><img src="/assets/images/3916582.png" alt="" /></div>
+             <div><img src="/assets/images/6422210.png" alt="" /></div>
+
+            
+           </div>
+           </div>
+
+           <div className='center-blog'>
+             <div className='center-blog-header'>
+             <div className='blog-header-left'>
+               <div className='header-img'>
+               <img src="/assets/images/logo.png" alt="" />
+               </div>
+               <div className='header-title'>
+                 <div className='title'>Markham Heid</div>
+                 <div>dfytukri</div>
+               </div>
+
+
+             </div>
+             <div className='blog-header-right'>
+               <div className='header-right-icons'>
+               <img src="/assets/images/3916580.png" alt="" />
+               </div>
+               <div className='header-right-icons'>
+               <div><img src="/assets/images/3916582.png" alt="" /></div>
+               </div>
+               <div className='header-right-icons'>
+               <img src="/assets/images/6422210.png" alt="" />
+               </div>
+               
+
+             </div>
+             </div>
+             <div className='main-center-cantainer'>
+             <div className='title'>
+             <span>How to Take a Break from Yourself</span> 
+             </div>
+             <div className='title-desc'>
+             <span>Escaping from your routines can help you escape from repetitive, <br/>unhelpful thoughts</span> 
+             </div>
+             <div className='center-desc-main'>
+
+
+
+               <div className='cntr-desc-img'>
+               <img src="/assets/images/dow.jpg" alt="" />
+
+               </div>
+               <div className='center-desc'>
+               Idon’t always find myself to be good company. Like a friend who only talks about his own hang-ups, my inner monologue can get pretty tiresome. Honestly, that again? Can’t we give it a rest?
+
+<br/>I think most of us feel this from time to time, this desire to get away from ourselves. But all the usual remedies — entertainment or exercise, alcohol or drugs — tend to work only in small doses, if they work at all. Rely on any of them too regularly and they become habits, which is often just a gentle synonym for compulsions.
+
+<br/>I’ve written before in this space about the brain-dulling effect of an overly routinized life. The novelist Rachel Cusk put a finer point on it when she wrote that “habits kill what is essential in ourselves.” If you’re feeling worn out by the stuff going on in your head, that’s a sure sign you need to change its inputs — give your mind something new to chew on so that it stops chewing on itself.
+
+<br/>Here, the research on vacations can be instructive.
+Idon’t always find myself to be good company. Like a friend who only talks about his own hang-ups, my inner monologue can get pretty tiresome. Honestly, that again? Can’t we give it a rest?
+
+<br/>I think most of us feel this from time to time, this desire to get away from ourselves. But all the usual remedies — entertainment or exercise, alcohol or drugs — tend to work only in small doses, if they work at all. Rely on any of them too regularly and they become habits, which is often just a gentle synonym for compulsions.
+
+<br/>I’ve written before in this space about the brain-dulling effect of an overly routinized life. The novelist Rachel Cusk put a finer point on it when she wrote that “habits kill what is essential in ourselves.” If you’re feeling worn out by the stuff going on in your head, that’s a sure sign you need to change its inputs — give your mind something new to chew on so that it stops chewing on itself.
+
+<br/>Here, the research on vacations can be instructive.
+
+               </div>
+
+             </div>
+             </div>
+
+          
+
+
+
+           
+              </div>
+
+
+           <div  className='right-blog'>
+             <div className='search'>
+             <input type="search" id="gsearch" name="gsearch" placeholder='Search'/>
+             </div>
+             <div className='right-img'>
+             <img src="/assets/images/mss.jpg" alt="" />
+
+             </div>
+             <div className='right-title'>
+             Markham Heid
+
+             </div>
+
+             <div className='right-followers'>
+               245K followers
+               </div>
+               <div className='right-desc'>
+               I write about health, science, and psychology. My work has appeared in Time magazine, the New York Times, Vice, and elsewhere. I live in Germany.
+
+               Follow
+               </div>
+               <div className='right-follow-button'>
+                
+                 <div><button className='follow-button' name="follow" type="submit" >Follow</button></div>
+                 <div className='follow-icon'><img src="/assets/images/3916631.png" alt="" /></div>
+                 
+
+               </div>
+
+
+              </div>
+
+        
+            </div>
+            
+            
+        )
+    }
+}
+
+
+export default BlogPage;
--- src/features/home/route.js
+++ src/features/home/route.js
@@ -1,5 +1,6 @@
 import { BlogPage, WelcomePage } from './';
 import SecondPage from './SecondPage';
+import  blog2  from './blog2';
 
 export default {
   path: '',
@@ -7,5 +8,6 @@ export default {
     { path: 'welcome', component: WelcomePage, isIndex: true },
     { path: 'second', component: SecondPage, isIndex: false },
     { path: 'blogs', component: BlogPage, isIndex: false },
+    { path: 'blogs2', component: blog2, isIndex: false },
   ],
 };
