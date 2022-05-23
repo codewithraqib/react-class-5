dcd40d7807cf00714bd0dd03095e867a65d313c6
aamir470 <suhailaamir778@gmail.com>
2022-05-23 15:10:35 +0530

    aamir changes

---
+++ src/features/home/Mypage.js
@@ -0,0 +1,263 @@
+import React from 'react';
+
+class Mypage extends React.PureComponent{
+    render (){
+        
+        return(
+         <div className="main-container">
+        <div className="left-section">
+          <div className="logo">
+            <img src="/assets/images/icons8-dots.png" alt="" />
+          </div>
+          <div className="left-section-links">
+            <div>
+              <img src="/assets/images/home.png" alt="" />
+            </div>
+            <div>
+              <img src="/assets/images/icons8-bell-100.png" alt="" />
+            </div>
+            <div>
+              <img src="/assets/images/icons8-bookmark.png" alt="" />
+            </div>
+            <div>
+              <img src="/assets/images/icons8-file.png" alt="" />
+            </div>
+            <div>
+              <img src="/assets/images/icons8-write.png" alt="" />
+            </div>
+          </div>
+        </div>
+
+
+
+
+        <div className="center-section">
+        <div className="center-section-header">
+            <div className="blog-header-left">
+              <div className="header-img">
+                <img src="/assets/images/DSC_E0387.jpg" alt="" />
+              </div>
+              <div className="header-title">
+                <div className="title">Aamir suhail</div>
+                <div>May25  .4min read *</div>
+              </div>
+            </div>
+            <div className="blog-header-right">
+              <div className="header-right-icons">
+                <img src="/assets/images/twitter.png" alt="" />
+              </div>
+              <div className="header-right-icons">
+                <div>
+                  <img src="/assets/images/snapechat.png" alt="" />
+                </div>
+              </div>
+              <div className="header-right-icons">
+                <img src="/assets/images/instagram.png" alt="" />
+              </div>
+              <div className="header-right-icons">
+              <img src="/assets/images/facebook.png" alt="" />
+
+              </div>
+            </div>
+          </div>
+
+          
+          
+          <div className="main-center-cantainer">
+            <div className="title">
+              <span>7 Best React UI Library You Should Use in Your Next Project</span>
+            </div>
+            <div className="title-desc">
+              <span>
+              Use these React UI libraries to create an awesome app today
+              </span>
+            </div>
+            <div className="center-desc-main">
+              <div className="cntr-desc-img">
+                <img src="/assets/images/coding1.jpg" alt="" />
+              </div>
+              <div className="cnter-desc">
+              Creating a UI from scratch sometimes can become time-consuming. Why write custom functionality when a library can do it for you? Developers’ best friends and life savors are libraries. A good project, in my opinion, makes use of some of the best libraries available. I’ve compiled a list of 7 React UI libraries that will help you to create an awesome React app in less time
+              </div>
+            </div>
+          </div>
+          <div className='react-info-cards'>
+            <div className='title'>
+              <span> 1. MIUI CORE</span>
+              </div>
+              <div className='react-img'>
+                <img src='/assets/images/MIUI.png' alt=''/>
+                </div>
+                <div className='desc'>
+                  <span>This is one of my favorite. I use this in most of my projects. MUI Core (formerly Material-UI) is an open-source project that helps React developers implement Google’s material design guidelines. It has over 78K stars on Github and is a popular UI design framework among React developers. So I believe there is not much reason for not using this.
+
+                   </span>
+
+                </div>
+              
+                
+
+
+            <div className='title'>
+              <span>2. REACT BOOTSTRAP</span>
+              </div>
+              <div className='react-img'>
+                <img src='/assets/images/reactbstrp.png' alt=''/>
+                </div>
+                <div className='desc'>
+                  <span>This is one of my favorite. I use this in most of my projects. MUI Core (formerly Material-UI) is an open-source project that helps React developers implement Google’s material design guidelines. It has over 78K stars on Github and is a popular UI design framework among React developers. So I believe there is not much reason for not using this.
+
+                   </span>
+
+                </div>
+              
+            <div className='title'>
+              <span>3. CHAKRA UI</span>
+              </div>
+              <div className='react-img'>
+                <img src='/assets/images/codewritten.png' alt=''/>
+                </div>
+                <div className='desc'>
+                  <span>This is one of my favorite. I use this in most of my projects. MUI Core (formerly Material-UI) is an open-source project that helps React developers implement Google’s material design guidelines. It has over 78K stars on Github and is a popular UI design framework among React developers. So I believe there is not much reason for not using this.
+
+                   </span>
+
+                </div>
+              
+           </div>
+           <div className='bottom-info'></div>
+           <div className='authour-info'></div>
+           <div className='title-desc-img'></div>
+           <div className='date-icon'></div>
+           <div  className="last-info">
+             
+           read more from java script
+           </div>
+
+          </div>
+  
+
+
+
+
+
+          <div className="right-section">
+            
+            <button>Get started</button>
+            <span>sign in</span>
+            <div className='search'>
+          <div className="search box">
+            <input type="search" id="gsearch" name="gsearch" placeholder="Search" />
+          </div>
+          <div className='search-icon'>
+               <img src='/assets/images/icons8-search-480.png' alt=''/>
+               </div>
+               </div>
+
+
+
+          <div className="right-img">
+            <img src="/assets/images/DSC_E0387.jpg" alt="" />
+          </div>
+             
+          <div className="right-title">Aamir Suhail</div>
+              
+          <div className="right-followers">2k followers</div>
+          <div className="right-desc">
+          Believes in learn by doing . Currently working as a software engineer in kashmir . Passionate about mobile application development .
+          </div>
+          <div className="right-follow-button">
+            <div>
+              <button className="follow-button" name="follow" type="submit">
+                Follow
+              </button>
+            </div>
+            <div className="follow-icon">
+              <img src="/assets/images/3916631.png" alt="" />
+            </div>
+          </div>
+          <div className="title-Medium">More from Medium</div>
+          <div className="right-cards">
+            <div className="head">
+              <div className="icon-name">
+                <div className="icon">
+                  <img src="/assets/images/IMG_1903.jpg" alt="" />
+                </div>
+                <div className="name">
+                  
+                  <span>faisal ayub</span>
+                </div>
+              </div>
+
+              <div className="description">
+                
+                <span> what is new in react</span>
+              </div>
+            </div>
+            <div className="image-container">
+              <div className="right-blog-img">
+                <img src="/assets/images/IMG_1903.jpg" alt="" />{' '}
+              </div>
+            </div>
+          </div>
+          
+          <div className="right-cards">
+            <div className="head">
+              <div className="icon-name">
+                <div className="icon">
+                  <img src="/assets/images/IMG_1903.jpg" alt="" />
+                </div>
+                <div className="name">
+                  
+                  <span>raqib yusuf</span>
+                </div>
+              </div>
+
+              <div className="description">
+                
+                <span> what is new in react 2</span>
+              </div>
+            </div>
+            <div className="image-container">
+              <div className="right-blog-img">
+                <img src="/assets/images/IMG_1903.jpg" alt="" />{' '}
+              </div>
+            </div>
+          </div>
+          
+          <div className="right-cards">
+            <div className="head">
+              <div className="icon-name">
+                <div className="icon">
+                  <img src="/assets/images/IMG-1903.jpg" alt="" />
+                </div>
+                <div className="name">
+                  
+                  <span>sameer</span>
+                </div>
+              </div>
+
+              <div className="description">
+                
+                <span> what is new in react 3</span>
+              </div>
+            </div>
+            <div className="image-container">
+              <div className="right-blog-img">
+                <img src="/assets/images/IMG_1903.jpg" alt="" />{' '}
+              </div>
+            </div>
+          </div>
+          </div>
+        
+        </div>
+
+       
+
+        
+           
+        
+        )
+    }
+}
+ export default Mypage;
