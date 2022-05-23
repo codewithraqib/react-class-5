e6850c74aeeeee21c2fd2439cca11a055b093072
aamir470 <suhailaamir778@gmail.com>
2022-05-23 15:28:46 +0530

    amir new changes

--- src/features/home/index.js
+++ src/features/home/index.js
@@ -1,7 +1,4 @@
 export { default as App } from './App';
 export { default as WelcomePage } from './WelcomePage';
-export { default as SecondPage } from './SecondPage';
 export { default as MyComponent } from './MyComponent';
-export { default as BlogPage } from './BlogPage';
-export { default as Blog2 } from './Blog2';
-export { default as ContacUs } from './../common/ContactUs';
+export {default as Mypage} from './Mypage';
--- src/features/home/route.js
+++ src/features/home/route.js
@@ -1,15 +1,9 @@
-import { BlogPage, WelcomePage,  } from './';
-import SecondPage from './SecondPage';
-import  Blog2  from './Blog2';
-import  ContactUs  from './../common/ContactUs';
+import {  WelcomePage, Mypage } from './';
 export default {
   path: '',
   childRoutes: [
     { path: 'welcome', component: WelcomePage, isIndex: true },
-    { path: 'second', component: SecondPage, isIndex: false },
-    { path: 'blogs', component: BlogPage, isIndex: false },
-    { path: 'blogs2', component: Blog2, isIndex: false },
-    { path: 'ContactUs', component: ContactUs, isIndex: false },
+    {path: 'mypage1', component: Mypage, isIndex: false},
 
   ],
 };
