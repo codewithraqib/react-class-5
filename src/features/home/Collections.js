a56ba1c61053cd20f62ca46877b992dd188c26df
unknown <bhatashu666@gmail.com>
2022-05-27 12:17:54 +0530

    collections page added

--- src/features/home/Collections.js
+++ src/features/home/Collections.js
@@ -2,7 +2,74 @@ import React from 'react';
 
 class Collections extends React.PureComponent {
   render() {
-    return <div>I am collection page</div>;
+    return (
+      <div className="collection-container ">
+        <div className="product-title">
+          <span>PRODUCTS</span>
+        </div>
+
+        <div className='content-wrapper'>
+        <div className="product-container">
+          <div className="product-cards">
+            <div className="show-main-desc">
+              <div>Showing 1–12 of 68 results</div>
+              <div className='sorting-btn'>
+                
+                
+                
+                <select name="sorting" id="sorting">
+                <option>default sorting:</option>
+                  <option >sort by latest</option>
+                  <option >sort by price low to high</option>
+                  <option >sort by price high to low </option>
+                 
+                </select>
+              </div>
+            </div>
+
+            <div className="product-inner-cards">
+              <div className="product-card1">
+                <div className="card-img">
+                  <img src="/assets/images/clth.jpeg" />
+                </div>
+                <div className="card-title">C A R D T I T L E</div>
+                <div className="card-desc">card-desc filter-container</div>
+              </div>
+
+              <div className="product-card1">
+                <div className="card-img">
+                  <img src="/assets/images/clth.jpeg" />
+                </div>
+                <div className="card-title">C A R D T I T L E</div>
+                <div className="card-desc">card-desc filter-container</div>
+              </div>
+
+              <div className="product-card1">
+                <div className="card-img">
+                  <img src="/assets/images/clth.jpeg" />
+                </div>
+                <div className="card-title">C A R D T I T L E</div>
+                <div className="card-desc">card-desc filter-container</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="filter-container">
+            <div className="filter-inner">filter-container</div>
+            <div className="links-container">
+              <div className="link">Home</div>
+              <div className="link">Clothing</div>
+              <div className="link">Shawls</div>
+              <div className="link">Home Decor</div>
+            </div>
+          </div>
+        </div>
+
+        </div>
+
+        
+      </div>
+    );
   }
 }
 
