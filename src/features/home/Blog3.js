7d182cfcc3a3a66e17c24f7f616000318bbc4aa1
Faisal Dar <faisudar24@gmail.com>
2022-06-02 12:56:40 +0530

    ssm page created

---
+++ src/features/home/Blog3.js
@@ -0,0 +1,206 @@
+
+
+ 
+ import React from 'react';
+
+
+class Blog3 extends React.PureComponent{
+
+   
+   
+    render(){
+
+        return(
+            <div className='main-container'>
+                <div className='icon-panel'>
+
+                    <img src="/assets/images/homeicon.png" alt=''/>
+                    <img src="/assets/images/bell.png" alt=''/>
+                    <img src="/assets/images/save.png" alt=''/>
+                    <img src="/assets/images/story.png" alt=''/>
+                    <img src="/assets/images/write.png" alt=''/>
+              
+                   </div>
+
+
+                 <div className='Author-info'>
+                  <div className='left-info'>
+                  <img src="/assets/images/girl.jpg" alt=''/>
+                  <div className='desc'>
+                  <span className='title' > hello world</span>
+                  <span className='description'> my name is hhhhh</span>
+                  </div>
+              
+                    </div>
+                  
+                  
+                  <div className='right-info'>
+                      
+                      <img src="/assets/images/twitter.png" alt=''/>
+                      <img src="/assets/images/facebook.png" alt=''/>
+                    <img src="/assets/images/linkedin.png" alt=''/>
+                    <img src="/assets/images/copy.png" alt=''/>
+                    <img src="/assets/images/write.png" alt=''/>
+                    
+
+                     </div>
+
+                     <div className='middle-container' content-wrapper >
+                       
+                   
+                   
+                   <div className='title'>
+                  
+                     <span>
+                    
+                          Standing Tall in the Era of iPosture 
+                       
+                       </span>
+
+                     
+
+                      
+                   </div>
+
+                      <div className='detail'>
+                           <span>
+
+                           How technology is reshaping our bodies and what we can do about it
+                           
+                           </span>
+
+                       </div>
+
+                       <div className='image-container'>
+
+                       <img src="/assets/images/faisal.jpg" alt=''/>
+                           
+                       </div>
+                      
+                      
+                       <div className='Middle-description-container'>
+                         <span>According to the Pew Research Center, it is estimated there are over 5 billion people who own mobile phones. With over half of the world’s population engaging with electronic devices, the spotlight has been on what technology can do for us, rather than what it is doing to us. Consequently, there has been less thought on what happens to our bodies while using handheld devices.</span>
+
+                       </div>
+                       <div className='image-container-icon'>  
+                       <div className='images1'>
+                      <img src="/assets/images/clap.png" alt='' />
+                      <img src="/assets/images/reply.png" alt='' />
+                      </div>
+                      <div className='images2'>
+                      <img src="/assets/images/share.png" alt='' />
+                      <img src="/assets/images/save.png" alt='' />
+
+                      </div>
+                     
+                      
+
+                    </div>
+
+                   
+
+                   <div className='middle-line'>
+
+                     </div>
+                       
+                       
+
+
+
+
+
+               </div>
+
+           </div>
+
+
+
+         <div className='side-panel'>
+
+      
+            <div className='search-items'>
+
+
+            <button> get started</button> 
+            <span className='description'> sign in</span>
+    
+ 
+           </div>
+           <div className='search-container'>
+               <div className='search-icon'>
+                   <img src="/assets/images/search.png" alt='' />
+               </div>
+             <input type='search' name='search' placeholder='search' />
+              </div>
+              <div className='right-panel-info'>
+                  <img src="/assets/images/girl.jpg" alt=''/>
+                  <div className='desc'>
+                  <span className='title' > faisal ayub</span><br></br>
+                  <span className='description'>1k followers</span> <br></br>
+                  <span className='description'>I write and teach about the mind-body connection and its relationship to health and well-being. More at https://www.bodyandposture.com/</span>
+                  </div>
+                  </div>
+                  <div className='follow-tag' >
+                      <button> follow</button>
+                      <div className='follow-img'>
+                      <img src="/assets/images/bell.png" alt='' />
+                      </div>
+                        
+                      </div>
+
+                    
+                      <div className='related-blogs-container'>
+
+                      <div className='desc1'>
+                  
+                  <span className='title' > More from Medium 1</span><br></br>
+                  <img src="/assets/images/train.jpeg" alt=''/>
+                  <span className='description'>1k followers</span> <br></br>
+                  <span className='description'>I write and teach about the mind-body connection and its relationship to health and well-being. More at https://www.bodyandposture.com/</span>
+                  
+                  </div>
+                      <div className='desc2'>
+                  
+                  <span className='title' > More from Medium 2</span><br></br>
+                  <img src="/assets/images/women.jpeg" alt=''/>
+                  <span className='description'>1k followers</span> <br></br>
+                  <span className='description'>I write and teach about the mind-body connection and its relationship to health and well-being. More at https://www.bodyandposture.com/</span>
+                  
+                  </div>
+                  <div className='desc3'>
+                  
+                  <span className='title' > More from Medium 3</span><br></br>
+                  <img src="/assets/images/people.jpg" alt=''/>
+                  <span className='description'>1k followers</span> <br></br>
+                  <span className='description'>I write and teach about the mind-body connection and its relationship to health and well-being. More at https://www.bodyandposture.com/</span>
+                  
+                  </div>
+
+
+                        
+                      </div>
+                  
+                 
+                     </div>
+           
+            
+               </div>
+
+
+             
+              
+
+
+        
+
+
+
+
+
+        )
+
+
+
+    }
+}
+export default Blog3;
---
+++ src/features/home/code1.js
@@ -0,0 +1,300 @@
+import React from 'react';
+
+
+class code1 extends React.PureComponent{
+
+
+    render(){
+
+
+        return(
+            <div className='Main-container'>
+
+    <div className='header'>
+    <div className='header1'>
+ <span>Approved by AICTE, Affiliated with University of Kashmir
+</span>
+
+    </div>
+    <div className='header2'>
+  
+     ISO 9001:2015 Certified
+  
+    </div>         
+
+    </div>
+    <div className='college-info-panel'>
+
+        <div className='image-description-container'>
+        <div className='ssmlogo'> 
+        <img src="/assets/images/ssmlogo.png" alt="" />
+         </div>
+
+            <div className='desc'>
+            <span className='title'> SSM COLLEGE OF ENGINEERING</span><br></br>
+           <span> SSM COLLEGE OF ENGINEERING</span><br></br><br></br>
+           <span className='title'> SSM COLLEGE OF ENGINEERING</span><br></br>
+            </div>
+         
+           </div>
+
+           <div className='phone-no'>
+            <div className='phone-image'>
+            <img src="/assets/images/phone.png" alt="" />
+                </div>   
+           <div className='no2title'>
+           <span className='title'> Call Us</span><br></br><br></br>
+           <span>91 9906413186</span>
+           </div>
+          
+
+
+               
+           </div>
+          
+          
+           <div className='email-address'>
+            <div className='email-image'>
+            <img src="/assets/images/email.jpg" alt="" />
+                </div>   
+         <div className='no3title'>
+         <span className='title'> Mail Us</span><br></br><br></br>
+           <span>ssmKashmir@gmail.com</span>
+
+         </div>
+          
+
+
+               
+           </div>
+
+           <div className='right-info-panel'>
+
+
+            <div className='information-button'>
+            <button> Admission open</button> 
+           </div>
+            <div className='information-button'>
+            <button> Login to ERP</button> 
+           </div>
+            <div className='information-button'>
+            <button> Online Fee Payment</button> 
+           </div>
+
+
+           </div>
+
+          
+</div>
+
+<div className='line-panel-1'>
+
+    <span> HOME</span>
+    <span> ABOUT SSM</span>
+    <span> ACADEMICS</span>
+    <span> DEPARMENTS</span>
+    <span> LIBRARY</span>
+    <span> TRAINING PLACEMENTS</span>
+    <span> GALLERY</span>
+    <span> CIEE</span>
+    <span> IQAC</span>
+    <span> NIRF</span>
+    <span> ALUMNI</span>
+    <span> VISITORS</span>
+    <span> CONTACT</span>
+    
+    
+    
+</div>
+<div className='image-container'>
+<img src="/assets/images/ssm.jpg" alt="" />
+
+
+</div>
+
+<div className='line-panel-2'>
+<div className='notification-button'>
+            <button> <span className='title'> Notifications</span><br></br>
+            <span className='title'>  See All    </span></button> 
+           </div>
+
+
+    <div className='description3'>
+        <span>Producing highly motivated quality engineers, technocrats and managers with sound knowledge and profound skill through efficient and effective teaching learning process.</span>
+    </div>
+   
+    </div>
+    
+    <div className='image-container-2'>
+    < span>Fostering creativity and growth of excellence for techno-economic upliftment of society </span>
+
+    <img src="/assets/images/ssm.jpg" alt="" /> 
+  
+    </div>
+    <div className='image-container-3'>
+    
+    <img src="/assets/images/ssm.jpg" alt="" /> 
+    <span>
+        <ol>
+            <li> Fostering creativity and growth of excellence for techno-economic upliftment of society</li><br></br>
+            <li> Fostering creativity and growth of excellence for techno-economic upliftment of society</li><br></br>
+            <li> Fostering creativity and growth of excellence for techno-economic upliftment of society</li><br></br>
+            <li> Fostering creativity and growth of excellence for techno-economic upliftment of society</li><br></br>
+            
+            </ol>
+            
+            
+            </span>
+
+  
+    </div>
+    
+    <div className='courses-offered-title'>
+
+       <span className='title'> COURSES OFFERED </span> 
+    </div>
+
+    <div className='courses-offered-container'>
+      
+      <div className='under-graduate-container'>
+
+      <div className='under-graduate'>
+      <div className='courses-offered-button'>
+            <button> <span className='title'> Under Graduate</span></button> 
+           </div>
+
+           <div className='course-name-1'>
+           <span className='title'> Bachelors in Civil Engineering</span>
+           <span> intake of 180 seats</span><br></br>
+           <span className='title'> Bachelors in Civil Engineering</span>
+           <span> intake of 180 seats</span><br></br>
+           <span className='title'> Bachelors in Civil Engineering</span>
+           <span> intake of 180 seats</span><br></br>
+           <span className='title'> Bachelors in Civil Engineering</span>
+           <span> intake of 180 seats</span><br></br>
+           <span className='title'> Bachelors in Civil Engineering</span>
+           <span> intake of 180 seats</span><br></br>
+           <span className='title'> Bachelors in Civil Engineering</span>
+           <span> intake of 180 seats</span>
+          
+
+
+           </div>
+           
+  
+      
+
+      </div>
+
+      </div>
+     
+     <div className='post-graduate-container'>
+         <div className='post-graduate'>
+         <div className='courses-offered-button'>
+            <button> <span className='title'> Post Graduate</span></button> 
+           </div>
+           <div className='course-name-2'>
+           <span className='title'> M.tech in Electonics Communications Engineering</span>
+           <span> intake of 18 seats</span><br></br>
+           <span className='title'> M.Tech in Mechanical Engineering</span>
+           <span> intake of 18 seats</span><br></br>
+           <span className='title'> M.Tech in Bussiness Administration</span>
+           <span> intake of 60 seats</span><br></br>
+           <span className='title'> Master of Computer Applicatioins</span>
+           <span> intake of 60 seats</span><br></br>
+           <span className='title'> Master of Science in Physics</span>
+           <span> intake of 15 seats</span><br></br>
+           <span className='title'> Master of Science in Electronics</span>
+           <span> intake of 15 seats</span>
+          
+
+
+           </div>
+           
+  
+
+
+         </div>
+
+        
+     </div>
+
+    </div>
+    <div className='image-container-4'>
+
+        <div className='title-description'>
+        <div className='titledesc'>
+    < span className='title'> Learn About Us </span><br></br>
+    <div className='desc'>
+    <span>
+    <ol>
+            <li> Fostering creativity and growth of excellence for techno-economic upliftment of society</li><br></br>
+            <li> Fostering creativity and growth of excellence for techno-economic upliftment of society</li><br></br>
+            <li> Fostering creativity and growth of excellence for techno-economic upliftment of society</li><br></br>
+            <li> Fostering creativity and growth of excellence for techno-economic upliftment of society</li><br></br>
+            
+            </ol>
+    
+
+    </span>
+   
+    
+    </div>
+   
+     </div>
+ 
+ </div>
+    
+    <img src="/assets/images/ssm.jpg" alt="" /> 
+
+   </div>
+    <div className='image-container-5'>
+
+        <div className='title-description'>
+        <div className='titledesc'>
+    < span className='title'> Learn About Us </span><br></br>
+    <div className='desc'>
+    <span>
+    <ol>
+            <li> Fostering creativity and growth of excellence for techno-economic upliftment of society</li><br></br>
+            <li> Fostering creativity and growth of excellence for techno-economic upliftment of society</li><br></br>
+            <li> Fostering creativity and growth of excellence for techno-economic upliftment of society</li><br></br>
+            <li> Fostering creativity and growth of excellence for techno-economic upliftment of society</li><br></br>
+            
+            </ol>
+    
+
+    </span>
+    
+    
+    </div>
+   
+     </div>
+ 
+ </div>
+
+
+
+    <img src="/assets/images/ssm.jpg" alt="" /> 
+
+   </div>
+
+
+
+
+    <div className='Events'> 
+      
+      <span className='title'>  Events and Updates  hhhh</span>
+    
+    </div>
+    
+    
+    </div>
+
+    )
+
+
+    }
+
+}
+export default code1;
