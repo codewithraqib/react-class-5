6a0b3412004aebfd247110bd4a784e777f692452
unknown <bhatashu666@gmail.com>
2022-06-06 11:35:34 +0530

    new files added

--- src/features/home/WelcomePage.js
+++ src/features/home/WelcomePage.js
@@ -1,68 +1,253 @@
 import React from 'react';
-import { AboutUs, ContactUs, MainStructuralComponent } from '../common';
+import { AboutUs, ContactUs } from '../common';
 import Header from '../common/Header';
-import MyComponent from './MyComponent';
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
+import SimpleImageSlider from 'react-simple-image-slider';
 
 class WelcomePage extends React.PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
-      services: [
-        { id: 0, name: 'Website Development', desc: 'we develop custome websites' },
-        { id: 1, name: 'Mobile App Development', desc: 'we develop custom Complex Mobile apps' },
-        { id: 2, name: 'Certification Courses', desc: 'we develop custom Complex Mobile apps' },
-        { id: 3, name: 'General Services', desc: 'we develop custom Complex Mobile apps' },
+      images: [
+        { url: '/assets/images/director.jpg' },
+        { url: '/assets/images/dow.jpg' },
+        { url: '/assets/images/co.jpg' },
       ],
-
-      teamMembers: [
-        { id: 0, name: 'name 1', desc: 'Website Dev' },
-        { id: 1, name: 'Name 2', desc: 'Mobile apps developer' },
-        { id: 2, name: 'Name 3', desc: 'Mobile apps developer' },
-        { id: 3, name: 'Name 4', desc: 'Mobile apps developer' },
-      ],
-      projects: [
-        { id: 0, name: 'project 1', desc: 'Website Dev' },
-        { id: 1, name: 'project 2', desc: 'Mobile apps developer' },
-        { id: 2, name: 'project 3', desc: 'Mobile apps developer' },
-        { id: 3, name: 'project 4', desc: 'Mobile apps developer' },
-      ],
+      glance: [{ id: 0, img: '/assets/images/dow.jpg', name: 'ashu' }],
     };
+    console.log('Props in wellcomePage are----====', this.state);
   }
+
+  goToShop = () => {
+    this.props.history.push('shop');
+  };
 
   render() {
     return (
       <div className="home-welcome-page">
-        {/* <MyComponent message={'I am from welcome page'} /> */}
+        <div className="main-slider">
+          <SimpleImageSlider
+            width={'100%'}
+            height={600}
+            images={this.state.images}
+            showBullets={false}
+            showNavs={false}
+            autoPlay
+            slideDuration={3}
+          />
+        </div>
 
-        <AboutUs />
+        <div className="glance-conatainer content-wrapper">
+          <div className="inner-container">
+            <div className="overlay-cards">
+              <div className="glance-img">
+                <img src="/assets/images/dow.jpg" alt="" />
+              </div>
+              <div className="overlay-text">
+                <span>overlay</span>
+              </div>
+            </div>
 
-        <MainStructuralComponent
-          title={'Service Offered'}
-          desc={
-            'We offer a lot of services in the field of IT which satisfy our customers and help our customers to grow.'
-          }
-          services={this.state.services}
-        />
+            <div className="overlay-cards">
+              <div className="glance-img">
+                <img src="/assets/images/dow.jpg" alt="" />
+              </div>
+              <div className="overlay-text">
+                <span>overlay</span>
+              </div>
+            </div>
 
-        <MainStructuralComponent
-          title={'Our Team'}
-          desc={
-            'We are 4 members in a team working in dynamic and different spheres of development'
-          }
-          teamMembers={this.state.teamMembers}
-        />
+            <div className="overlay-cards">
+              <div className="glance-img">
+                <img src="/assets/images/dow.jpg" alt="" />
+              </div>
+              <div className="overlay-text">
+                <span>overlay</span>
+              </div>
+            </div>
 
-        <MainStructuralComponent
-          title={'Our Projects | Products'}
-          desc={
-            'We have developed a lot of products for ourselves and more for clients in which some prominent ones are '
-          }
-          projects={this.state.projects}
-        />
-        <ContactUs/>
+            <div className="overlay-cards">
+              <div className="glance-img">
+                <img src="/assets/images/dow.jpg" alt="" />
+              </div>
+              <div className="overlay-text">
+                <span>overlay</span>
+              </div>
+            </div>
+
+            <div className="overlay-cards">
+              <div className="glance-img">
+                <img src="/assets/images/dow.jpg" alt="" />
+              </div>
+              <div className="overlay-text">
+                <span>overlay</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* new */}
+
+        <div className="contact-notification-container content-wrapper">
+          <div className="contact-view-card">
+            <div className="contact-title">
+              <span>INTERNATIONAL DELHI PUBLIC SCHOOL</span>
+            </div>
+            <div className="location-map">
+              <div>
+                <span className="map-icon">
+                  <img src="/assets/images/location.png" alt="" />
+                </span>
+                <span className="map-address"> Chowdhary Bagh, Srinagar, J&K, 19000</span>
+              </div>
+            </div>
+            <div className="mail-icon">
+              <span>
+                <img src="/assets/images/mail.png" alt="" />
+              </span>
+              <span className="mail-address">idpssrinagar@gmail.com</span>
+            </div>
+            <div className="phone-icon">
+              <span>
+                <img src="/assets/images/phone.png" alt="" />
+              </span>
+              <span className="phone-address"> 8899888393</span>
+            </div>
+          </div>
+
+          <div className="notifications-card">
+            <marquee behavior="scroll" direction="up">
+              <div className="scroll-notification">
+                <div className="notification">
+                  <span>
+                    If now feels like one of those times for you, keep reading. In this guide, we’ll
+                    show you how to generate scrolling text in HTML and CS
+                  </span>
+                </div>
+              </div>
+            </marquee>
+            <div className="notification-title">Notification</div>
+          </div>
+        </div>
+
+        <div className="premier-container content-wrapper">
+          <div className="preimer-title">IDPS PREIMER</div>
+          <div className="premier-main-cards">
+            <div className="premier-card">
+              <div className="card-gift">
+                <img src="/assets/images/idps.png" alt="" />
+              </div>
+              <div className="premier-name">
+                <span> About School</span>
+              </div>
+              <div className="premier-desc">
+                <span>
+                  {' '}
+                  The School Campus The School Campus is situated on the outskirts of Srinagar city,
+                  about 12 kms from city centre Lal Chowk, and 2 kms from Kashmir University campus,
+                  sprawled over 100 kanals of land.
+                </span>
+              </div>
+            </div>
+
+            <div className="premier-card">
+              <div className="card-gift">
+                <img src="/assets/images/idps.png" alt="" />
+              </div>
+              <div className="premier-name">
+                <span> About School</span>
+              </div>
+              <div className="premier-desc">
+                <span>
+                  {' '}
+                  The School Campus The School Campus is situated on the outskirts of Srinagar city,
+                  about 12 kms from city centre Lal Chowk, and 2 kms from Kashmir University campus,
+                  sprawled over 100 kanals of land.
+                </span>
+              </div>
+            </div>
+
+            <div className="premier-card">
+              <div className="card-gift">
+                <img src="/assets/images/idps.png" alt="" />
+              </div>
+              <div className="premier-name">
+                <span> About School</span>
+              </div>
+              <div className="premier-desc">
+                <span>
+                  {' '}
+                  The School Campus The School Campus is situated on the outskirts of Srinagar city,
+                  about 12 kms from city centre Lal Chowk, and 2 kms from Kashmir University campus,
+                  sprawled over 100 kanals of land.
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="trending-container content-wrapper">
+          <div className='trend-tiltle'>
+          Trending at IDPS
+          </div>
+
+          <div className='trend-main-card'>
+            <div className='trend-inner'>
+            <div className="social-img">
+                <img src="/assets/images/facebook.png" alt="" />
+              </div>
+              <div className='social-view'>
+              <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FIdpssrinagar%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+               width={340}
+                height={500}
+                 border="none"
+                 overflow="hidden"
+                 scrolling="no" 
+                frameborder="0"
+                 allowfullscreen="true"
+                 allow="autoplay;
+               encrypted-media; picture-in-picture; "
+               >
+
+               </iframe>
+              </div>
+            </div>
+            
+            <div className='trend-inner'>
+            <div className="social-img">
+                <img src="/assets/images/instagram.png" alt="" />
+              </div>
+              <div className='social-view'>
+              <iframe src="https://www.instagram.com/bhat__ashu/embed"
+              width={300} 
+              height={500}
+              frameborder="0"
+               scrolling="no" allowtransparency="true"></iframe>
+
+               
+              </div>
+            </div>
+
+            <div className='trend-inner'>
+            <div className="social-img">
+                <img src="/assets/images/twitter.png" alt="" />
+              </div>
+              <div className='social-view'>
+              <iframe 
+              border="none"
+               height={500}
+               width={340}
+               data-tweet-url="https://twitter.com/IdpsSrinagar" 
+              src="data:text/html;charset=utf-8,%3Ca%20class%3D%22twitter-timeline%22%20href%3D%22https%3A//twitter.com/IdpsSrinagar%3Fref_src%3Dtwsrc%255Etfw%22%3ETweets%20by%20IdpsSrinagar%3C/a%3E%0A%3Cscript%20async%20src%3D%22https%3A//platform.twitter.com/widgets.js%22%20charset%3D%22utf-8%22%3E%3C/script%3E%0A"></iframe>
+             
+              </div>
+            </div>
+       
+
+          </div>
+        </div>
       </div>
-      
     );
   }
 }
