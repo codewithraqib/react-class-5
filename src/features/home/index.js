eb504f4a1c6ff1ac427e3150c3d2d69af6599df1
unknown <bhatashu666@gmail.com>
2022-07-19 10:39:42 +0530

    new branch epaper

--- src/features/home/index.js
+++ src/features/home/index.js
@@ -1,5 +1,3 @@
 export { default as App } from './App';
 export { default as WelcomePage } from './WelcomePage';
-export { default as ContacUs } from './../common/ContactUs';
-export { default as NewPage } from './NewPage';
-export { default as FullNews } from './FullNews';
+
--- src/features/home/route.js
+++ src/features/home/route.js
@@ -1,14 +1,8 @@
 import { WelcomePage,  } from './';
-import  ContactUs  from './../common/ContactUs';
-import NewPage from './NewPage';
-import FullNews from './FullNews';
 export default {
   path: '',
   childRoutes: [
     { path: 'welcome', component: WelcomePage, isIndex: true },
-    { path: 'ContactUs', component: ContactUs, isIndex: false },
-    { path: 'newpage', component: NewPage, isIndex: false },
-    { path: 'fullnews', component: FullNews, isIndex: false },
 
 
   ],
